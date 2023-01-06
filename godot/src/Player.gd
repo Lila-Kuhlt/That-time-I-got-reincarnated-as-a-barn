9a9c303ce9f0667b7032dfa56f9e3ee4c2cfbc80
nat-rix <upezu@student.kit.edu>
2023-01-07 00:54:05 +0100

    Add sprite flipping for player

--- godot/src/Player.gd
+++ godot/src/Player.gd
@@ -1,17 +1,24 @@
 extends KinematicBody2D
 
+onready var anim := $AnimationRoot/AnimationPlayer
+onready var sprite := $AnimationRoot/PlayerSprite
 
 func _ready():
 	pass
 
 
 func _process(delta):
-	var dir_x = Input.get_axis("left", "right")
-	var dir_y = Input.get_axis("up", "down")
-	var dir = Vector2(dir_x, dir_y)
+	var dir_x := Input.get_axis("left", "right")
+	var dir_y := Input.get_axis("up", "down")
+	var dir := Vector2(dir_x, dir_y)
 	if dir.x == 0 && dir.y == 0:
-		$AnimationRoot/AnimationPlayer.current_animation = "idle"
+		anim.current_animation = "idle"
 	else:
-		$AnimationRoot/AnimationPlayer.current_animation = "walk"
+		anim.current_animation = "walk"
+	if dir.x > 0:
+		sprite.flip_h = false
+	elif dir.x < 0:
+		sprite.flip_h = true
+
 	move_and_collide(dir)
 	pass
