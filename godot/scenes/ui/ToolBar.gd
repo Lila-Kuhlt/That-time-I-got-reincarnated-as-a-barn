41df0602c89db27b3701defb0caa71b1974366c9
nat-rix <upezu@student.kit.edu>
2023-01-07 13:51:39 +0100

    Implement scrolling by using mouse wheel

--- godot/scenes/ui/ToolBar.gd
+++ godot/scenes/ui/ToolBar.gd
@@ -1,14 +1,31 @@
 extends HBoxContainer
 
-var selected_item := 0
+var selected_item: int = 0
+var selected_item_subspace: float = 0
 onready var child_count := get_child_count()
 
 func _ready():
-	update_selected_item()
+	update_selected_item(true)
 
 func _process(delta):
-	pass
+	var mouse_scroll_left := Input.is_action_just_released("scroll_left_mouse")
+	var mouse_scroll_right := Input.is_action_just_released("scroll_right_mouse")
+	# TODO: scrolling with joypad with different action
+	#       beacause scroll buttons don't support Input.get_axis :(
+	var scroll := float(mouse_scroll_right) - float(mouse_scroll_left)
+	if scroll != 0:
+		selected_item_subspace += scroll
+		update_selected_item()
 
-func update_selected_item():
+func update_selected_item(force=false):
+	if selected_item_subspace < 0:
+		selected_item_subspace = child_count + selected_item_subspace
+	elif selected_item_subspace >= child_count:
+		selected_item_subspace -= child_count
+	var new_selected_item: int = round(selected_item_subspace)
+	new_selected_item %= child_count
+	if !force and new_selected_item == selected_item:
+		return
+	selected_item = new_selected_item
 	for child in range(child_count):
 		get_child(child).set_selected(child == selected_item)
