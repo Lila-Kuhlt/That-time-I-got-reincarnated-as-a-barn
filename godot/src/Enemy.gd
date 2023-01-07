4d6d0f3de0373b1e1fb7e26472d7bdafd8f6c7cd
isiko404 <isihd.ko@gmail.com>
2023-01-07 12:55:48 +0100

    Add Pathfinding

--- godot/src/Enemy.gd
+++ godot/src/Enemy.gd
@@ -1,16 +1,48 @@
-extends Node2D
+extends KinematicBody2D
 
+const MAX_SPEED = .5
+const DEFAULT_POS = Vector2(100.0, 100.0)
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var current_target:Node2D = null
 
+export var path := NodePath()
+
+onready var _agent = $NavigationAgent2D
+
+func set_target(node: Node2D):
+	pass
+
+func _process(delta):
+	# Update Goal
+	if current_target != null:
+		_agent.set_target_location(current_target.global_position)
+		pass
+	
+	if not _agent.is_navigation_finished():
+		var next_location = _agent.get_next_location()
+		var dir := position.direction_to(next_location)
+		var velocity = dir * MAX_SPEED
+		_agent.set_velocity(velocity)
+		move_and_collide(dir)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_agent.set_target_location(DEFAULT_POS)
 	pass # Replace with function body.
+	
+func _get_current_arraction(node: Node2D) -> int:
+	if node == null:
+		return 0
+	elif node.get_parent().is_in_group(("Tower")):
+		return 1
+	elif node.get_parent().is_in_group(("Player")):
+		return 2
+	return 1
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+# Check if the Player is still targeted if he respawns
+func _on_Area2D_area_entered(area: Node2D):
+	print("test")
+	if _get_current_arraction(area) > _get_current_arraction(current_target):
+		print("test2")
+		current_target = area
+		_agent.set_target_location(current_target.global_position)
