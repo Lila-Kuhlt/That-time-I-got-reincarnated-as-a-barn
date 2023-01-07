50b61958d181d12b372304ec89215c7b53dae9d2
EliasSchaut <eschaut@outlook.de>
2023-01-07 18:01:25 +0100

    hotfix

--- godot/scenes/ui/UI.gd
+++ godot/scenes/ui/UI.gd
@@ -5,8 +5,6 @@ extends CanvasLayer
 # var a = 2
 # var b = "text"
 signal screen_clicked (worldpos) 
-signal hover_start
-signal hover_stop
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,10 +19,3 @@ func _ready():
 func _on_TextureButton_button_up():
 	var click_pos = get_viewport().get_mouse_position()
 	emit_signal("screen_clicked", click_pos)
-
-
-func _on_TextureButton_mouse_entered():
-	emit_signal("hover_start")
-
-func _on_TextureButton_mouse_exited():
-	emit_signal("hover_stop")
--- godot/src/World.gd
+++ godot/src/World.gd
@@ -6,6 +6,7 @@ extends Node2D
 onready var Map = $Map
 const Tower = preload("res://scenes/Tower.tscn")
 var last_tower = null
+var last_tower_location = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,18 +19,13 @@ func _on_UI_screen_clicked(worldpos):
 	tower.global_position = map_pos
 	tower.is_active = true
 	tower.modulate.a = 1
-	
-func _on_UI_hover_start():
-	$Timer.start()
 
-func _on_UI_hover_stop():
-	$Timer.stop()
-
-func _on_Timer_timeout():
-	if last_tower != null:
-		remove_child(last_tower)
+func _process(delta):
 	var map_pos = Map.world_to_map_pos(get_viewport().get_mouse_position())
-	var tower : Node2D = Tower.instance()
-	last_tower = tower
-	add_child(tower)
-	tower.global_position = map_pos
+	if last_tower_location != map_pos:
+		remove_child(last_tower)
+		var tower : Node2D = Tower.instance()
+		last_tower = tower
+		add_child(tower)
+		tower.global_position = map_pos
+	last_tower_location = map_pos
