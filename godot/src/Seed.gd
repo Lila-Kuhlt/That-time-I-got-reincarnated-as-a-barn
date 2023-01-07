1cc621795cf973b7ff27a49bb63b371b2e624735
isiko404 <isihd.ko@gmail.com>
2023-01-07 17:10:40 +0100

    Make Seed growable

--- godot/src/Seed.gd
+++ godot/src/Seed.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 var state = 0
 
-const STATE_MAX = 4
-const MIN_GROW_TIME = 5
-const MAX_GROW_TIME = 10
+const MAX_STATE = 3
+const MIN_GROW_TIME = 1
+const MAX_GROW_TIME = 1
+const FINAL_FORM_MULT = 4
 
 signal on_grow (state)
 
@@ -19,19 +20,24 @@ func _ready():
 	emit_signal("on_grow", state)
 
 func _on_grow():
-	if active:
-		state += 1
-		if state < STATE_MAX:
-			sprite.set_frame(state)
-			print("Grow")
-			_update_time()
-			emit_signal("on_grow", state)
-		else:
-			active = false
-			emit_signal("on_grow", -1)
-			sprite.visible = false
+	if not active:
+		return
+	state += 1
+	sprite.set_frame(state)
+	if state <= MAX_STATE - 1:
+		_update_time()
+		emit_signal("on_grow", state)
+	if state >= MAX_STATE:
+		active = false
+		emit_signal("on_grow", -1)
+		timer.stop()
+		print("DONE GROWING")
 		
 func _update_time():
 	var new_duration = rand_range(MIN_GROW_TIME, MAX_GROW_TIME)
-	timer.wait_time = new_duration
+	
+	if state == MAX_STATE -1:
+		new_duration = new_duration * FINAL_FORM_MULT
+		print("FINAL_FORM")
 	print(new_duration)
+	timer.start(new_duration)
