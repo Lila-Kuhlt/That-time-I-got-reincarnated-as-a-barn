6c04b4383e67d9fb26dec61940893b4109250430
Robin Ebert <ebertrobin2002@gmail.com>
2023-01-07 22:58:44 +0100

    Prevent projectiles from coliding

--- godot/src/Projectile.gd
+++ godot/src/Projectile.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-
 var speed: float = 200
 var damage: float = 1.0
 var piercing: int = 1 # the maximum amount of enemies the projectile can hit in a row
 var target: Vector2
+
+const enemy_class = preload("res://src/Enemy.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,6 +23,8 @@ func _physics_process(delta: float):
 func _on_enemy_hit(area):
 	# collision layer 14 is for projectile-enemy collision
 	var enemy = area.get_parent()
+	if not enemy is enemy_class:
+		return
 	enemy.damage(damage)
 	piercing -= 1
 	if piercing == 0:
