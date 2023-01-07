8188dcdb4844069a1ac84290f363b1ee2a39e8a9
konsumlamm <konsumlamm@gmail.com>
2023-01-07 16:45:38 +0100

    Free projectiles when they reach their target

--- godot/src/Projectile.gd
+++ godot/src/Projectile.gd
@@ -1,18 +1,19 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-export (int) var speed = 200
-onready var target = global_position
-var velocity = Vector2()
+var speed: float = 200
+var damage: float = 1
+var target: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func shoot_target(target_pos):
+func shoot_target(target_pos: Vector2):
 	target = target_pos
 
-func _physics_process(delta):
-	velocity = global_position.direction_to(target) * speed
-	global_position += velocity * delta
+func _physics_process(delta: float):
+	if global_position.is_equal_approx(target):
+		# TODO: damage to enemies
+		queue_free()
+	global_position = global_position.move_toward(target, speed * delta)
