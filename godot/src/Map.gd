20161ae98f66544150065ac2108123d5aaae78db
nat-rix <upezu@student.kit.edu>
2023-01-07 03:02:14 +0100

    Generate background layer randomly

--- godot/src/Map.gd
+++ godot/src/Map.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
+export var grass_tile_ratio: float = 0.4;
+export var tile_count_w: int = 50;
+export var tile_count_h: int = 30;
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var bg_layer = $BackgroundLayer;
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print(bg_layer.get_cell_autotile_coord(0, 0))
+	generate_bg_layer()
 
+func _process(delta):
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+func generate_bg_layer():
+	bg_layer.clear()
+	for y in range(tile_count_h):
+		for x in range(tile_count_w):
+			var id = int(randf() > grass_tile_ratio)
+			bg_layer.set_cell(x, y, 0, false, false, false, Vector2(id, 0))
