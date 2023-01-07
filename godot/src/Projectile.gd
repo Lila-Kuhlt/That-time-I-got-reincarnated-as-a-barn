1df20093e66e6dde39b14007d86977f2c17f950a
EliasSchaut <eschaut@outlook.de>
2023-01-07 02:06:49 +0100

    add first tower logic + create projetile scene

--- godot/src/Projectile.gd
+++ godot/src/Projectile.gd
@@ -4,20 +4,13 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-const Projectile = preload("res://scenes/Projectile.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#    pass
-
-
-func _on_Area2D_body_entered(body):
-	var projectile = Projectile.instance()
-	add_child(projectile)
-	pass # Replace with function body.
+#	pass
