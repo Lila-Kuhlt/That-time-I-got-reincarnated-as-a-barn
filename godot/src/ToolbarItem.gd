64aaa270a38d4e5f5cf9df8d3af1349a8deb6b4a
isiko404 <isihd.ko@gmail.com>
2023-01-08 16:39:42 +0100

    Fix Unused Toolbar Items

--- godot/src/ToolbarItem.gd
+++ godot/src/ToolbarItem.gd
@@ -6,6 +6,25 @@ signal item_selected(slot_id)
 
 var slot_id: int
 
+const DEFAULT_TEXTURE = -1
+
+var TEXTURE_LOOKUP = {
+	# Items
+	Globals.ItemType.ToolScythe : 0,
+	Globals.ItemType.ToolWateringCan : 1,
+	
+	#Plants
+	Globals.ItemType.PlantChili : 2,
+	Globals.ItemType.PlantTomato : 3,
+	Globals.ItemType.PlantAubergine : 4,
+	Globals.ItemType.PlantPotato : 5,
+	
+	# Towers
+	Globals.ItemType.TowerWindmill : -1,
+	Globals.ItemType.TowerWatertower : -1,
+	Globals.ItemType.TowerWIP : -1
+}
+
 func _ready():
 	set_item()
 	pass
@@ -13,12 +32,16 @@ func _ready():
 func set_selected(is_sel: bool) -> void:
 	self.pressed = is_sel
 
-func set_item(item_id=Globals.ITEM_NAMES.size()):
-	$ItemTexture.visible = false
-	if item_id != -1:
+func set_item(item_id=DEFAULT_TEXTURE):
+	if not item_id in Globals.ITEM_NAMES:
+		item_id = DEFAULT_TEXTURE
+
+	if item_id != DEFAULT_TEXTURE:
 		$ItemTexture.visible = true
-		var x: int = $ItemTexture.texture.region.size.y * int(item_id)
+		var x: int = $ItemTexture.texture.region.size.y * int(TEXTURE_LOOKUP[item_id])
 		$ItemTexture.texture.region.position.x = x
+	else:
+		$ItemTexture.visible = false
 
 func register_callback(toolbar):
 	toolbar.connect("item_selected", self, "_on_toolbar_selection_changed")
