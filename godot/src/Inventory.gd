cd08137edf0a334d2a58572e59f49b3fd84f9ac8
Angelo Aracri <angeloaracri@yahoo.de>
2023-01-09 06:07:40 +0100

    Add Inventory and Cost data structures

---
+++ godot/src/Inventory.gd
@@ -0,0 +1,35 @@
+extends Node
+
+signal inventory_changed(inventory)
+
+export var initial_inventory = {
+	Globals.ItemType.PlantChili : 1,
+	Globals.ItemType.PlantTomato : 2,
+	Globals.ItemType.PlantAubergine : 3,
+	Globals.ItemType.PlantPotato : 4
+}
+
+var _inventory = {}
+
+func _ready():
+	# initialize data structure
+	_inventory = {}
+	for type in Globals.ItemType:
+		_inventory[type] = initial_inventory.get(type, 0)
+	
+func can_pay(cost_inventory) -> bool:
+	for type in _inventory.keys():
+		if cost_inventory._inventory[type] < _inventory[type]:
+			return false
+	return true
+
+func pay(cost_inventory):
+	for type in _inventory.keys():
+		_inventory[type] -= cost_inventory._inventory[type]
+	emit_signal("inventory_changed", self)
+
+func try_pay(cost_inventory) -> bool:
+	if can_pay(cost_inventory):
+		pay(cost_inventory)
+		return true
+	return false
