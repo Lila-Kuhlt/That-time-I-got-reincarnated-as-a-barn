627caabb216d4c838368d470edb933dcded7a654
Robin Ebert <ebertrobin2002@gmail.com>
2023-01-09 14:34:57 +0100

    Allow adding items to inventory

--- godot/src/Inventory.gd
+++ godot/src/Inventory.gd
@@ -3,18 +3,17 @@ extends Node
 signal inventory_changed(inventory)
 
 export var initial_inventory = {
-	Globals.ItemType.PlantChili : 1,
-	Globals.ItemType.PlantTomato : 2,
-	Globals.ItemType.PlantAubergine : 3,
-	Globals.ItemType.PlantPotato : 4
+	Globals.ItemType.PlantChili : 5,
+	Globals.ItemType.PlantTomato : 5,
+	Globals.ItemType.PlantAubergine : 5,
+	Globals.ItemType.PlantPotato : 5
 }
 
 var _inventory = {}
 
 func _ready():
 	# initialize data structure
-	_inventory = {}
-	for type in Globals.PLANTS:
+	for type in initial_inventory.keys():
 		_inventory[type] = initial_inventory.get(type, 0)
 	
 	# Sry about that
@@ -22,24 +21,27 @@ func _ready():
 	emit_signal("inventory_changed", self)
 	
 func can_pay(cost_inventory) -> bool:
-	for type in _inventory.keys():
+	for type in cost_inventory.get_keys():
 		if cost_inventory._inventory[type] > _inventory[type]:
 			return false
 	return true
 
-func pay(cost_inventory):
-	for type in _inventory.keys():
-		_inventory[type] -= cost_inventory.get_value(type)
+func _modify(modification_inventory, remove : bool = false):
+	var multiplier = -1 if remove else 1
+	for type in modification_inventory.get_keys():
+		assert(modification_inventory.get_value(type) >= 0)
+		_inventory[type] += multiplier * modification_inventory.get_value(type)
 	emit_signal("inventory_changed", self)
 
-func try_pay(cost_inventory) -> bool:
-	if can_pay(cost_inventory):
-		pay(cost_inventory)
-		return true
-	return false
+func pay(cost_inventory):
+	assert(can_pay(cost_inventory))
+	_modify(cost_inventory, true)
 
 func get_keys() -> Array:
 	return _inventory.keys()
 	
 func get_value(key):
 	return _inventory.get(key, 0)
+
+func add(add_inventory):
+	_modify(add_inventory)
