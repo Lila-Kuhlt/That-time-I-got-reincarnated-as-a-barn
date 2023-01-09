04ed933be66d21e17efe2f9069a96b82b5e47d21
nat-rix <upezu@student.kit.edu>
2023-01-09 20:47:46 +0100

    More main MainMenu

---
+++ godot/scenes/ui/ButtonQuit.gd
@@ -0,0 +1,12 @@
+extends Button
+
+
+# Declare
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_ButtonQuit_pressed():
+	get_tree().quit()
