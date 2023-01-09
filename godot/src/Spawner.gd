dd9d0e4a0d7f69e2562277e8c29374b544961cf4
Aaron Geiger <aaron@geigr.dev>
2023-01-09 10:38:30 +0100

    Add cooldown decrease over time

--- godot/src/Spawner.gd
+++ godot/src/Spawner.gd
@@ -2,10 +2,14 @@ extends StaticBody2D
 
 const ENEMY_PRELOAD = preload("res://scenes/Enemy.tscn")
 
-export (int) var spawn_radius = 1
-export (float) var spawn_probability_per_tick = 0.0876 # = ~0.6 per second
-export (int) var ticks_per_second : int = 10
-export (float) var cooldown_in_secs = 3.2
+export (int) 	var spawn_radius = 1
+export (float) 	var spawn_probability_per_tick = 0.0876 # = ~0.6 per second
+export (int) 	var ticks_per_second : int = 10
+
+# Cooldown related
+export (float) 	var cooldown_in_secs = 3.2
+export (float) 	var spawn_probability_increase = 0.2 # per second
+export (float)	var min_cooldown = 0.5
 
 var _tick_time : float
 var _cooldown_counter: float = 0
@@ -43,6 +47,8 @@ func _do_tick():
 		_cooldown_counter = 0
 
 func _physics_process(delta):
+	cooldown_in_secs -= spawn_probability_increase * delta
+	cooldown_in_secs = max(0, cooldown_in_secs)
 	if _has_cooldown:
 		_cooldown_counter += delta
 		if _cooldown_counter >= cooldown_in_secs:
